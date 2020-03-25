c81958009db0992b3c143b7e28a0eeef1eac7bde
roberalm <robe_r96@hotmail.es>
2020-03-25 11:25:32 +0100

    Initial commit

---
+++ Menu_Clases/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//Roberto Alcazar Morales
+//
+//
+//
+
+//Prepara un programa que permita acceder a las siguientes funciones:
+//-    Menú de opciones:
+//-    Añadir un dato
+//-    Mostrar todos los datos
+//-    Mostrar datos a partir de un determinado dato de tamaño
+//-    Búsqueda parcial a partir de una letra o subconjunto de letras(en el caso de encontrar ese fichero deberá mostrar todos los datos que le pertenecen)
+//-    Mensaje de error, dato introducido incorrecto
+//-    Salir del menú
+namespace Menu_Clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
